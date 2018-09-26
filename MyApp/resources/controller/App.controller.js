79587dcf4a9e2e164f7b4be1a102353b41db6228
vsenn <valery.semichev@gmail.com>
2018-09-26 11:16:40 +0300

    week 2 unit 2

--- MyApp/resources/controller/App.controller.js
+++ MyApp/resources/controller/App.controller.js
@@ -1,10 +1,14 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"opensap/myapp/model/formatter"
+], function (Controller, MessageToast, formatter) {
 		"use strict";
 		
 		return Controller.extend("opensap.myapp.controller.App", {
+			
+			formatter : formatter,
+			
 			onShowHello : function () {
 				// read msg from i18n model
 				var oBundle = this.getView().getModel("i18n").getResourceBundle();
