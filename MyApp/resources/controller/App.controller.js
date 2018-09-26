562b945b1eb7040cd4d9f1155e486c05b9f3c206
vsenn <valery.semichev@gmail.com>
2018-09-26 13:48:45 +0300

    week 2 unit 4

--- MyApp/resources/controller/App.controller.js
+++ MyApp/resources/controller/App.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",	
 	"opensap/myapp/model/formatter"
-], function (Controller, MessageToast, formatter) {
+], function (Controller, MessageToast, Filter, FilterOperator, formatter) {
 		"use strict";
 		
 		return Controller.extend("opensap.myapp.controller.App", {
@@ -16,7 +18,22 @@ sap.ui.define([
 				var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 				// show message
 				MessageToast.show(sMsg);
+			},
+
+			onFilterProducts : function (oEvent) {
+				// build filter array
+				var aFilter = [], sQuery = oEvent.getParameter("query"),
+					// retrieve list control
+					oList = this.getView().byId("productsList"),
+					// get binding for aggregation 'items'
+					oBinding = oList.getBinding("items");
+	
+				if (sQuery) {
+					aFilter.push(new Filter("ProductID", FilterOperator.Contains, sQuery));
+				}
+				// apply filter. an empty filter array simply removes the filter
+				// which will make all entries visible again
+				oBinding.filter(aFilter);
 			}
 		});
-
 });
